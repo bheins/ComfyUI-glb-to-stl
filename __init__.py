8bfd965ba2029f63080d90cdb0fc17fcab7784f0
bheins <bheins@users.noreply.github.com>
2025-05-31 10:25:35 -0500

    add simplification

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .glb_converter import Mesh3DExporterNode
+from .mesh_simplifier import MeshSimplifierNode
 
 NODE_CLASS_MAPPINGS = {
-    "Mesh3DExporterNode": Mesh3DExporterNode
+    "Mesh3DExporterNode": Mesh3DExporterNode,
+    "MeshSimplifierNode": MeshSimplifierNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Mesh3DExporterNode": "Mesh3D Exporter"
+    "Mesh3DExporterNode": "Mesh3D Exporter",
+    "MeshSimplifierNode": "Mesh Simplifier"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
